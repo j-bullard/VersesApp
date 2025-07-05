f6be31044b89747807712be986f85d5e00979764
Jason Bullard <jason@bullard.io>
2025-07-05 14:43:07 -0500

    Initial Commit

---
+++ VersesApp/VersesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersesAppApp.swift
+//  VersesApp
+//
+//  Created by Jason Bullard on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct VersesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
